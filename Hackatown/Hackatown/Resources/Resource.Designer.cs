858822fe9a6f71d91ed44215c2ec9b8c98a298ef
Olivier Lauzon <olivier.lauzon@polymtl.ca>
2018-01-20 21:37:48 -0500

    Conco
    sisi senor

--- Hackatown/Hackatown/Resources/Resource.Designer.cs
+++ Hackatown/Hackatown/Resources/Resource.Designer.cs
@@ -45,16 +45,22 @@ namespace Hackatown
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int index = 2130837504;
+			public const int Concordia = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Oratoire = 2130837505;
+			public const int index = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int Poly = 2130837506;
+			public const int mcgill = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int Udem = 2130837507;
+			public const int Oratoire = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int Poly = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int Udem = 2130837509;
 			
 			static Drawable()
 			{
